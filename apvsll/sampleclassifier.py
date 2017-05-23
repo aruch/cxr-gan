ec7e3d8da502ab4c45bec4b94303eff040ab1748
willadu <william.a.du@gmail.com>
2017-05-23 15:43:38 -0700

    Adds image size constant to sample classifier

--- apvsll/sampleclassifier.py
+++ apvsll/sampleclassifier.py
@@ -2,9 +2,11 @@ import os
 import h5py
 from APLLClassifier import APvLLClassifier
 
-classifier = APvLLClassifier(64)
+IMG_SIZE = 64 
+
+classifier = APvLLClassifier(IMG_SIZE)
 f = h5py.File(os.path.join('/scratch/users/aruch/nerdd', "train.hdf5"))
-images = f["images" + str(64)]
+images = f["images" + str(IMG_SIZE)]
 
 
 newlabels = classifier.classify(images[1000:1010])
